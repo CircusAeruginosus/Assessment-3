0385373f70fd12c32f21c4e0f8209f946310bfe9
Caitlin Thompson <ctt00297@gmail.com>
2021-11-12 10:08:54 -0600

    part 2 finished

--- about.js
+++ about.js
@@ -5,10 +5,17 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Form submitted successfully!");
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let catPic = document.getElementById("cat")
+
+
+catPic.addEventListener("mouseover", function(event) {
+	alert("You seem like a nice person!")
+} )
