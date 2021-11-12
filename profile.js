a8f9e903d5987c153ffcd3517f4c3bc743eca745
Caitlin Thompson <ctt00297@gmail.com>
2021-11-12 11:01:50 -0600

    part 4 finished

---
+++ profile.js
@@ -0,0 +1,21 @@
+// let catPic = document.getElementById("cat")
+
+
+// catPic.addEventListener("mouseover", function(event) {
+// 	alert("You seem like a nice person!")
+// } )
+
+let colorBtn = document.getElementById("color")
+colorBtn.addEventListener("click", function(event) {
+    alert("Mustard Yellow")
+})
+
+let placeBtn = document.getElementById("place")
+placeBtn.addEventListener("click", function(event) {
+    alert("Schrute Farms")
+} )
+
+let ritualBtn = document.getElementById("ritual")
+ritualBtn.addEventListener("click", function(event) {
+    alert("Belsnickel")
+})
